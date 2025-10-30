27bbf93c23349bfbdc0da9c5e8106da269f570d5
seanblatter <63274835+seanblatter@users.noreply.github.com>
2025-10-29 19:01:52 -0500

    Support dynamic emoji shopping and refresh home header

--- lettuce-clicker/app/(tabs)/garden.tsx
+++ lettuce-clicker/app/(tabs)/garden.tsx
@@ -14,6 +14,7 @@ export default function GardenScreen() {
     placeEmoji,
     updatePlacement,
     clearGarden,
+    registerCustomEmoji,
   } = useGame();
 
   return (
@@ -27,6 +28,7 @@ export default function GardenScreen() {
         placeEmoji={placeEmoji}
         updatePlacement={updatePlacement}
         clearGarden={clearGarden}
+        registerCustomEmoji={registerCustomEmoji}
       />
     </SafeAreaView>
   );
